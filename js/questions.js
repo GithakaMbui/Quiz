e25b71c93a44c938f3d8c1187a6224150d5a58d1
Githaka <githakamaru@gmail.com>
2022-05-23 07:53:43 +0300

    simple quiz app for the uongozi project

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the chairman of IEBC?",
+    answer: "Wafula Chebukati",
+    options: [
+      "Raila Odinga",
+      "William Ruto",
+      "Wafula Chebukati",
+      "Uhuru Kenyatta"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The parliament of Kenya consists of how many houses?",
+    answer: "two",
+    options: [
+      "one",
+      "two",
+      "three",
+      "four"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The national assembly consists of how many members?",
+    answer: "three hundred and fifty",
+    options: [
+      "two hundred and ninety",
+      "fourty seven",
+      "three hundred and fifty",
+      "four hundred"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many women representatives are there in the national assembly?",
+    answer: "forty seven",
+    options: [
+      "forty seven",
+      "forty eight",
+      "forty nine",
+      "two hundred"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The national assembly consists of how many members?",
+    answer: "67",
+    options: [
+      "47",
+      "48",
+      "290",
+      "67"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
